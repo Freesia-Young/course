f1dff7f3b710d71bfba8c124089f1195eec45c47
ygx <15217695618@163.com>
2021-05-26 20:55:47 +0800

    1.增加business模块，并增加初始启动代码

---
+++ business/src/main/java/com/course/business/config/BusinessApplication.java
@@ -0,0 +1,28 @@
+package com.course.business.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+
+//配置扫描从主包，或者直接配server包只是启动项目成功，但实际请求需要使用mapper时又报错了
+@SpringBootApplication(scanBasePackages = {"com.course"})
+//@SpringBootApplication(scanBasePackages = {"server"})
+@EnableEurekaClient
+@MapperScan("com.course.server.mapper")
+public class BusinessApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+	public static void main(String[] args) {
+
+		SpringApplication app = new SpringApplication(BusinessApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Business地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
+
+}
+
