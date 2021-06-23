06a08ba8f60b427674dc5cfc82905ca3817f2d4a
ygx <1220572026@qq.com>
2021-06-23 21:20:24 +0800

    1.增加vue多环境配置，后端地址为读环境变量process.env.VUE_APP_SERVER

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -120,7 +120,7 @@ export default {
     list(page) {
       let _this = this;
       Loading.show();
-      _this.$ajax.post('http://localhost:9000/business/admin/chapter/list', {
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/chapter/list', {
         page: page,
         size: _this.$refs.pagination.size
       }).then((respond) => {
@@ -145,7 +145,7 @@ export default {
       }
 
       Loading.show();
-      _this.$ajax.post('http://localhost:9000/business/admin/chapter/save',
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/chapter/save',
           _this.chapter).then((response) => {
         Loading.hide();
         console.log("保存大章列表结果", response);
@@ -166,7 +166,7 @@ export default {
       let _this = this;
       Confirm.show("删除大章后不可恢复，确认删除？", function (){
         Loading.show();
-        _this.$ajax.delete('http://localhost:9000/business/admin/chapter/delete/' + id).then((res)=>{
+        _this.$ajax.delete(process.env.VUE_APP_SERVER + '/business/admin/chapter/delete/' + id).then((res)=>{
           Loading.hide();
           console.log("删除大章结果：",res);
           if (res.data.success){
